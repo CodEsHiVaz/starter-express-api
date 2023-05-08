7ec74463e66876cd0e0c49a64e2672491cbb1250
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2023-05-08 22:55:52 +0530

    test added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+app.get("/test", (req, res) => {
+  try {
+    res.send("Hello");
+  } catch (error) {
+    res.send(error);
+  }
+});
+app.listen(process.env.PORT || 3000);
