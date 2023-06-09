65436eba481c4e0b4218a8dd3e4abf23f545ef3e
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2023-06-09 17:28:25 +0530

    project added

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-require('dotenv').config();
-
-const MongoDB_URL = process.env.MongoDB_URL;
-console.log(MongoDB_URL);
-const connection =  mongoose.connect(MongoDB_URL);
-
-module.exports = { connection };
--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-const express = require('express');
-const cors = require('cors');
-const connection = require('./config/db');
-const blogsRouter = require('./routes/blogs.route');
-require('dotenv').config();
+const express = require("express");
+const dbConnection = require("./src/config/DBconfig");
+const router = require("./src/routes/routes");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const Port = process.env.PORT || 8080;
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
 
-const app = express(); //Server
-
-//middlewares
-app.use(cors());
+app.use("/temp", express.static(path.join(__dirname, "./temp")));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
-app.use(blogsRouter);
 
-//Read Data or Get Data
-app.get('/', (req, res) => {
-  res.send({ status: 'success', msg: 'Welcome to Homepage' });
-});
-
-//Route for wrong url
-app.all('*', (req, res) => {
-  res.status(404).send('Not Found');
-});
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, async () => {
+/* `app.all("*", (req, res) => {...})` is a middleware function that handles all requests that do not
+match any of the defined routes. It logs a message to the console indicating that the requested
+route is not found and sends a 404 status code with a message "REQUESTED ROUTE ARE NOT FOUND" to the
+client. This is useful for handling invalid requests and preventing the server from crashing. */
+/* for testing api response . */
+app.get("/", async (req, res) => {
   try {
-    await connection;
-    console.log('Connected to MongoDB');
-  } catch (e) {
-    console.log('ERROR: ' + e);
+    res.status(200).json({ message: "Hello, wellcome! to my site" });
+  } catch (error) {
+    console.log("app.get  error:", error);
+    res.status(500).json({ message: error.message });
   }
-  console.log(`server started on http://localhost:${PORT}`);
+});
+app.all("*", (req, res) => {
+  console.log("REQUESTED ROUTE ARE NOT FOUND");
+  res.status(404).send("REQUESTED ROUTE ARE NOT FOUND");
+});
+
+/* This code is starting the server and listening on a specified port (either the port specified in the
+environment variable `PORT` or port 8080 if `PORT` is not defined). When the server starts, it first
+waits for the database connection to be established (using the `await` keyword with the
+`dbConnection` promise), and then logs a message to the console indicating that the server is
+listening on the specified port. If there is an error while starting the server, it logs an error
+message to the console. */
+app.listen(Port, async () => {
+  try {
+    await dbConnection;
+
+    console.log(`listening on http://localhost:${Port}/`);
+  } catch (error) {
+    console.log("app.listen  error:", error);
+
+    console.log(`error while listening on ${Port}`);
+  }
 });
